8b08ed30f2604def69baf1d2d613ea82baa4db02
Trần Vũ Hoàn <99460453+TVHoan@users.noreply.github.com>
2023-06-30 10:27:23 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+// only need mysql OR sqlite
+// both are included here for reference
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	// "github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+var err error
+
+type Person struct {
+	ID        uint   `json:"id"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	City      string `json:"city"`
+}
+
+func main() {
+
+	// NOTE: See we're using = to assign the global var
+	//         	instead of := which would assign it only in this function
+	// db, err = gorm.Open("sqlite3", "./gorm.db")
+	// db, _ = gorm.Open("mysql", "root:1@tcp(127.0.0.1:3306)/database?charset=utf8mb4&parseTime=True&loc=Local")
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN: "host=localhost user=postgres password=1 dbname=gorm port=5432 sslmode=disable TimeZone=Asia/Shanghai", // data source name, refer https://github.com/jackc/pgx
+		PreferSimpleProtocol: true, // disables implicit prepared statement usage. By default pgx automatically uses the extended protocol
+	  }), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	// defer db.Close()
+
+	db.AutoMigrate(&Person{})
+
+	r := gin.Default()
+	r.GET("/people/", GetPeople)
+	r.GET("/people/:id", GetPerson)
+	r.POST("/people", CreatePerson)
+	r.PUT("/people/:id", UpdatePerson)
+	r.DELETE("/people/:id", DeletePerson)
+
+	r.Run(":8009")
+}
+
+func DeletePerson(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var person Person
+	d := db.Where("id = ?", id).Delete(&person)
+	fmt.Println(d)
+	c.JSON(200, gin.H{"id #" + id: "deleted"})
+}
+
+func UpdatePerson(c *gin.Context) {
+
+	var person Person
+	id := c.Params.ByName("id")
+
+	if err := db.Where("id = ?", id).First(&person).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	}
+	c.BindJSON(&person)
+
+	db.Save(&person)
+	c.JSON(200, person)
+
+}
+
+func CreatePerson(c *gin.Context) {
+
+	var person Person
+	c.BindJSON(&person)
+
+	db.Create(&person)
+	c.JSON(200, person)
+}
+
+func GetPerson(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var person Person
+	if err := db.Where("id = ?", id).First(&person).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		c.JSON(200, person)
+	}
+}
+func GetPeople(c *gin.Context) {
+	var people []Person
+	if err := db.Find(&people).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		c.JSON(200, people)
+	}
+
+}
